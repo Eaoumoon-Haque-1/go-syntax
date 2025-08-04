7f5728f87e995ca5f49625294c675467de124e9c
Eaoumoon Haque <22-47526-2@student.aiub.edu>
2025-08-04 19:07:47 +0600

    1st code

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt" //fmt=format
 
-func main(){//a new line here befor curly brace will result in error
+func main(){//a new line here before curly brace will result in error
 	fmt.Println("Hello World..!!!")
 	fmt.Printf("Let's deep dive in GO buddy\n")
 }
