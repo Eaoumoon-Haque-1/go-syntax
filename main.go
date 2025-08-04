4073d534136574f9fb6cbb0487b15eb71e1c793d
Eaoumoon Haque <22-47526-2@student.aiub.edu>
2025-08-04 19:04:22 +0600

    Running 1st programme

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt" //fmt=format
+
+func main(){//a new line here befor curly brace will result in error
+	fmt.Println("Hello World..!!!")
+	fmt.Printf("Let's deep dive in GO buddy\n")
+}
